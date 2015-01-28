2f2e5d8b769e5173ee20414b06c4b55a2c28f5f4
dgm1 <daniellemarcos@hotmail.com>
2015-01-27 20:20:34 -0800

    adding everything

---
+++ learning_journal/forms.py
@@ -0,0 +1,42 @@
+from wtforms import (
+    Form,
+    TextField,
+    TextAreaField,
+    validators,
+    PasswordField,
+)
+
+strip_filter = lambda x: x.strip() if x else None
+
+
+class EntryCreateForm(Form):
+    title = TextField(
+        'Entry title',
+        [validators.Length(min=1, max=255)],
+        filters=[strip_filter]
+    )
+    body = TextAreaField(
+        'Entry body',
+        [validators.Length(min=1)],
+        filters=[strip_filter]
+    )
+
+class EntryUpdateForm(Form):
+    title = TextField(
+        'Entry title',
+        [validators.Length(min=1, max=255)],
+        filters=[strip_filter]
+    )
+    body = TextAreaField(
+        'Entry body',
+        [validators.Length(min=1)],
+        filters=[strip_filter]
+    )
+
+class LoginForm(Form):
+    username = TextField(
+        'Username', [validators.Length(min=1, max=255)]
+    )
+    password = PasswordField(
+        'Password', [validators.Length(min=1, max=255)]
+    )
---
+++ learning_journal/scripts/initializedb.py
@@ -0,0 +1,44 @@
+from cryptacular.pbkdf2 import PBKDF2PasswordManager as Manager
+import os
+import sys
+import transaction
+
+from sqlalchemy import engine_from_config
+
+from pyramid.paster import (
+    get_appsettings,
+    setup_logging,
+    )
+
+from pyramid.scripts.common import parse_vars
+
+from ..models import (
+    DBSession,
+    MyModel,
+    Base,
+    User,
+    )
+
+
+def usage(argv):
+    cmd = os.path.basename(argv[0])
+    print('usage: %s <config_uri> [var=value]\n'
+          '(example: "%s development.ini")' % (cmd, cmd))
+    sys.exit(1)
+
+
+def main(argv=sys.argv):
+    if len(argv) < 2:
+        usage(argv)
+    config_uri = argv[1]
+    options = parse_vars(argv[2:])
+    setup_logging(config_uri)
+    settings = get_appsettings(config_uri, options=options)
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    with transaction.manager:
+        manager = Manager()
+        password = manager.encode(u'admin')
+        admin = User(name=u'admin', password=password)
+        DBSession.add(admin)
